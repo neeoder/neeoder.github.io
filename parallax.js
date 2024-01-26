d2b5f97eae84e1860e1ed45a72e02de4cea1bfc6
neeoder <102406914+neeoder@users.noreply.github.com>
2024-01-26 15:52:05 +0100

    Add files via upload

--- parallax.js
+++ parallax.js
@@ -1,12 +1,13 @@
 const parallax_el = document.querySelectorAll('.parallax');
 
-let xValue = 0, yValue = 0;
-
-window.addEventListener('mousemove', (e) => {
-    xValue = e.clientX - (window.innerWidth / 2);
-    yValue = e.clientY - (window.innerHeight / 2);
-
-    parallax_el.forEach((el) => {
-        el.style.transform = `translateX(calc(-50% + ${-xValue}px)) translateY(calc(-50% + ${yValue}px))`;
+const revealOnScroll = () => {
+    const scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+    parallax_el.forEach(el => {
+        if (scrollPos + window.innerHeight > el.offsetTop) {
+            el.classList.add('reveal');
+        }
     });
-});
+};
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
